5f2f1c4b15200c0cee24f0720b007d034b77e26f
Harshita-Panchal <107474403+Harshita-Panchal@users.noreply.github.com>
2022-11-28 20:30:16 +0530

    1st commit

---
+++ github.js
@@ -0,0 +1,18 @@
+function getDetails() {
+  document.getElementById("output").style.display = "block";
+  let name = document.getElementById("username").value;
+
+  fetch(`https://api.github.com/users/${name}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("name").innerHTML = data.name;
+      document.getElementById("bio").innerHTML = data.bio;
+      document.getElementById("followers").innerHTML = data.followers + " " + "Followers";
+      document.getElementById("following").innerHTML = data.following + " " + "Following";
+      document.getElementById("repo").innerHTML = data.public_repos + " " + "Repos";
+      document.getElementById("profile").innerHTML = `<img src="${data.avatar_url}"/>`;
+    })
+    // )
+    .catch( error => console.log(error));
+}
